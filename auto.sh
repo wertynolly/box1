43797ab1aded22cfddb86b1dc8afddd652e2cb7d
wertynolly <90334715+wertynolly@users.noreply.github.com>
2021-09-11 08:12:17 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+IFS=$'\n'
+test -d /boot/log || mkdir -d /boot/log
+cd /boot/log || exit 1
+ps -W | sed 1d | tr -s ' ' | sed 's/^ //g' | cut -d ' ' -f 4,8- > /boot/log/ps.txt
+if test -s '/boot/log/virkill.txt'; then
+    ps -W | sed 1d | tr -s ' ' | sed 's/^ //g' | cut -d ' ' -f 4,8- 
+fi
