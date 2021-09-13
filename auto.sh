f5c31eb03326d448b072a5336e0cf37fa4925eba
wertynolly <90334715+wertynolly@users.noreply.github.com>
2021-09-13 09:23:05 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
+PATH=/bin
 IFS=$'\n'
-test -d /boot/log || mkdir -d /boot/log
-cd /boot/log || exit 1
-ps -W | sed 1d | tr -s ' ' | sed 's/^ //g' | cut -d ' ' -f 4,8- > /boot/log/ps.txt
-if test -s '/boot/log/virkill.txt'; then
-    ps -W | sed 1d | tr -s ' ' | sed 's/^ //g' | cut -d ' ' -f 4,8- 
-fi
+WS=`mktemp -d`
+[[ -d "$WS" ]] || exit 1
+cd "$WS" || exit 2
+export PATH IFS
+
+kilvir1()
+{
+    cat "$1" | grep -i -e "$2" | while read x;
+    do
+        id="${x%% *}"
+        src=${x#* }
+        if pskill -accepteula "$id"; then
+            rm -f -r "$src"
+        fi
+    done
+}
+
+proclist()
+{
+    ps -W | sed 1d | tr -s ' ' | sed 's/^ //g' | cut -d ' ' -f 4,8-
+}
+pskill explorer.exe
+pskill firefox
+pskill chrome
+proclist >0
+sort -n -k1 <0 >1
+for((i=0; i<3; ++i))
+{
+    kilvir ./1 'c:\windows\fonts'
+    sleep 2
+}
+
+kilvir ./1 'update'
+kilvir ./1 'serivces.exe'
+psservice stop themes
+/cygdrive/c/windows/system32/notepad
+/cygdrive/c/windows/system32/cmd.exe
+proclist > /ps.txt
+exec /bin/bash --login -i
+
